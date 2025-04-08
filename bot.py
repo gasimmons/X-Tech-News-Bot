57826782f4b1f9dfd1bd47fc6de937acc9c31149
gasimmons <gasimmons@ucsd.edu>
2025-04-07 17:53:43 -0700

    Fixed .env

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
 import tweepy
 import os
 from dotenv import load_dotenv
+
+
+load_dotenv()
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+
+auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+api.update_status("Hello world from my bot!")
